ace22a1f7a32eb6017e5315e5518f37a839fcd78
AKavaa <alex.kavaleuskiy33@gmail.com>
2026-03-26 00:37:10 +0200

    feat: add GET request to retrieve data from a list

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks = [ # list of tasks
+    {"id": 1, "title": "Buy groceries", "completed": False},
+    {"id": 2, "title": "Learn FastAPI", "completed": False}
+]
+
 @app.get("/")
 async def root():
     return {"message": "Hello FASTAPI"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
