7c7214470f5c99f6c778dfe8ca5ae1d433233780
AKavaa <alex.kavaleuskiy33@gmail.com>
2026-03-26 00:47:38 +0200

    add a POST request, tested correctly and creates new object inside tasks list

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,22 @@ tasks = [ # list of tasks
     {"id": 2, "title": "Learn FastAPI", "completed": False}
 ]
 
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello FASTAPI"}
 
-@app.get("/tasks")
+@app.get("/tasks") # retrieve a list with data
 def get_tasks():
     return tasks
+
+@app.post("/tasks")
+def create_tasks(title:str):
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": title,
+        "completed": False
+    }
+    tasks.append(new_task)
+    return new_task
