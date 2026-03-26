becf148dfe61692d18ee3fadd0cf23e64e5cdda0
AKavaa <alex.kavaleuskiy33@gmail.com>
2026-03-26 18:49:47 +0200

    feat: Implemented DELETE feature

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ def create_tasks(task:TaskCreate):
     tasks.append(new_task)
     return new_task
 
-
+# Get the task with its unique ID
 @app.get("/tasks/{task_id}")
 def get_task(task_id:int):
     for task in tasks:
@@ -41,3 +41,14 @@ def get_task(task_id:int):
             return task
         
     raise HTTPException(status_code = 404, detail="Task with this ID doesnt exists" )
+
+# Delete the task by finding the ID and removing it 
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+             tasks.remove(task)
+             return {"message": "Task Deleted"}
+        
+        
+    raise HTTPException(status_code = 404, detail="Task with this ID alreayd deleted" )    
